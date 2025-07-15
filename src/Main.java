a7943709cc68816807427918223c4c3b4dd17963
KhalidDev <148249284+devAlq@users.noreply.github.com>
2025-07-15 20:55:18 +0300

    taskdone

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,29 @@ import java.util.Map;
 
 public class Main {
 
-    // * Create a HashMap called `memo` of type `Map<Integer, Integer>` to store the base case and the result of the recursive function.
-    // Your code here 
+    static HashMap<Integer, Integer> memo = new HashMap<>();
+    
 
     public static void main(String[] args) {
-        int initialTrees = 10; // Assuming the farmer starts with 1 tree
-        int days = 2; // Number of iterations
-        int result = doubleTrees(initialTrees, days);
+        int initialTrees = 10; 
+        int days =2; 
+        int result = doubleTrees(initialTrees, days , 1);
+        System.out.println(memo);
+
         System.out.println("Number of trees after " + days + " iterations: " + result);
     }
 
-    public static int doubleTrees(int initialTrees, int n) {
-        if (n == 0) {
+    public static int doubleTrees(int initialTrees, int n,int curentDay) {
+      
+        if (memo.get(curentDay) != null) {
+            return memo.get(curentDay);
+        }
+                
+        memo.put(curentDay, initialTrees );
+        
+        if (n == 1) {
             return initialTrees;
         }
-        // Check if the result is already in the memo
-        // Your code here 
-        
-        int result = 2 * doubleTrees(initialTrees, n - 1);
-
-        // Store the result of the recursive function in the `memo` HashMap.
-        // Your code here
-        
-        
-        return result;
+        return doubleTrees(initialTrees * 2, n - 1, curentDay + 1);
     }
 }
